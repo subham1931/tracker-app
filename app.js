8cf8e64113f5304a5be5ea8ad262f6bcf2712acb
Nobisuke19 <subham019650@gmail.com>
2024-08-24 10:16:26 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
+const server = http.createServer(app); // Create the HTTP server with Express app
+const io = socketio(server); // Attach Socket.IO to the server
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public'))); // Use 'app.use' instead of 'app.set' for static files
+app.use(express.static('public'));
+
+io.on('connection', function (socket) {
+    socket.on('send-location', function(data){
+        io.emit("receive-location", { id: socket.id, ...data }); // Corrected event name here
+    });
+    console.log('New user connected');
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+server.listen(port, () => { // Use 'server.listen' instead of 'app.listen'
+    console.log(`Server is running on port ${port}`);
+});
